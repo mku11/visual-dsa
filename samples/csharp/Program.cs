e31b6eb457fedb5adca0b8d1b329b50242876d44
Max Kas <93253421+mku11@users.noreply.github.com>
2026-03-27 12:58:58 +0200

    updated c# samples with individual launchers

--- samples/csharp/Program.cs
+++ samples/csharp/Program.cs
@@ -10,19 +10,31 @@ namespace Demo
         {
             // We need to register Linq for visual-dsa to work, any linq method will do
             System.Linq.Enumerable.Select(["RegisterLinq"], x => x);
-
-            // Arrays.RunMain(args);
-            // Lists.RunMain(args);
-            // Graphs.RunMain(args);
-            // Trees.RunMain(args);
-            // Hashes.RunMain(args);
-            // LinkedLists.RunMain(args);
-            // Primitives.RunMain(args);
-            // GraphMapNodes.RunMain(args);
-            // GraphsPrimitivesEdges.RunMain(args);
-            DoublyLinkedLists.RunMain(args);
-            // Queues.RunMain(args);
-            // Plot.RunMain(args);
+            
+            if (args.Length == 0 || args[0] == "Arrays")
+                Arrays.RunMain(args);
+            else if (args[0] == "Lists")
+                Lists.RunMain(args);
+            else if (args[0] == "Graphs")
+                Graphs.RunMain(args);
+            else if (args[0] == "Trees")
+                Trees.RunMain(args);
+            else if (args[0] == "Hashes")
+                Hashes.RunMain(args);
+            else if (args[0] == "LinkedLists")
+                LinkedLists.RunMain(args);
+            else if (args[0] == "Primitives")
+                Primitives.RunMain(args);
+            else if (args[0] == "GraphMapNodes")
+                GraphMapNodes.RunMain(args);
+            else if (args[0] == "GraphsPrimitivesEdges")
+                GraphsPrimitivesEdges.RunMain(args);
+            else if (args[0] == "DoublyLinkedLists")
+                DoublyLinkedLists.RunMain(args);
+            else if (args[0] == "Queues")
+                Queues.RunMain(args);
+            else if (args[0] == "Plot")
+                Plot.RunMain(args);
         }
     }
 }
