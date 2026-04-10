2a5068ce45a80c89946c4df53c6eb814c544a39c
Max Kas <93253421+mku11@users.noreply.github.com>
2026-04-10 14:57:51 +0300

    fixed pointer issue with cxx graphs sample

--- samples/cpp/src/Graphs.cpp
+++ samples/cpp/src/Graphs.cpp
@@ -16,14 +16,14 @@ class GraphNode
 {
 public:
     T value;
-    vector<GraphNode<T>> *children = new vector<GraphNode<T>>();
+    vector<GraphNode<T> *> *children = new vector<GraphNode<T> *>();
     vector<int> *childrenCost = new vector<int>();
     GraphNode() {}
     GraphNode(T value)
     {
         this->value = value;
     }
-    void add(GraphNode<T> node, int cost)
+    void add(GraphNode<T> *node, int cost)
     {
         this->children->push_back(node);
         this->childrenCost->push_back(cost);
@@ -38,8 +38,8 @@ template <typename T>
 class Graph
 {
 public:
-    GraphNode<T> root;
-    Graph(GraphNode<T> root)
+    GraphNode<T> *root;
+    Graph(GraphNode<T> *root)
     {
         this->root = root;
     }
@@ -51,53 +51,56 @@ public:
     void start()
     {
         // nodes of a graph
-        GraphNode<string> gnode0 = GraphNode<string>("0");
-        GraphNode<string> gnode1 = GraphNode<string>("1");
-        GraphNode<string> gnode2 = GraphNode<string>("2");
-        GraphNode<string> gnode3 = GraphNode<string>("3");
-        GraphNode<string> gnode4 = GraphNode<string>("4");
-        gnode0.add(gnode1, 10);
-        gnode0.add(gnode2, 20);
-        gnode2.add(gnode3, 15);
-        gnode3.add(gnode2, 10);
-        gnode3.add(gnode4, 25);
-        gnode1.add(gnode4, 45);
-        gnode4.add(gnode2, 55);
+        GraphNode<string> *gnode0 = new GraphNode<string>("0");
+        GraphNode<string> *gnode1 = new GraphNode<string>("1");
+        GraphNode<string> *gnode2 = new GraphNode<string>("2");
+        GraphNode<string> *gnode3 = new GraphNode<string>("3");
+        GraphNode<string> *gnode4 = new GraphNode<string>("4");
+        gnode0->add(gnode1, 10);
+        gnode0->add(gnode2, 20);
+        gnode2->add(gnode3, 15);
+        gnode3->add(gnode2, 10);
+        gnode3->add(gnode4, 25);
+        gnode1->add(gnode4, 45);
+        gnode4->add(gnode2, 55);
 
         // wrap to a graph
-        Graph<string> graph = Graph<string>(gnode0);
-        GraphNode<string> graphRoot = graph.root;
-        graphRoot.add(GraphNode<string>("5"), 11);
-        gnode1.add(GraphNode<string>("6"), 12);
-        gnode2.add(GraphNode<string>("7"), 13);
+        Graph<string> *graph = new Graph<string>(gnode0);
+        GraphNode<string> *graphRoot = graph->root;
+        graphRoot->add(new GraphNode<string>("5"), 11);
+        gnode1->add(new GraphNode<string>("6"), 12);
+        gnode2->add(new GraphNode<string>("7"), 13);
 
         bfs(gnode0);
 
         cout << "done" << endl;
+
+        // generally this is where you should be releasing
+        // the allocated memory
     }
 
-    void bfs(GraphNode<string> node)
+    void bfs(GraphNode<string> *node)
     {
-        queue<GraphNode<string>> q = queue<GraphNode<string>>();
-        q.push(node);
+        queue<GraphNode<string> *> *q = new queue<GraphNode<string> *>();
+        q->push(node);
         set<string> v = set<string>();
         int steps = 0;
-        int size = q.size();
-        while (q.size() > 0)
+        int size = q->size();
+        while (q->size() > 0)
         {
-            GraphNode<string> x = q.back();
+            GraphNode<string> *x = q->back();
             size--;
-            for (GraphNode<string> child : *(x.children))
+            for (GraphNode<string> *child : *(x->children))
             {
-                if (v.count(child.value) > 0)
+                if (v.count(child->value) > 0)
                     continue;
-                q.push(child);
-                v.insert(child.value);
+                q->push(child);
+                v.insert(child->value);
             }
             if (size == 0)
             {
                 steps++;
-                size = q.size();
+                size = q->size();
             }
         }
     }
